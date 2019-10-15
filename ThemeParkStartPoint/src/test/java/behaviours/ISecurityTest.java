81b8eb4bfcf51093c055f8b115a703a7528a0dfc
Suzanne Dyson <suzanne.dyson2334@gmail.com>
2019-10-15 22:42:28 +0100

    initial commit

---
+++ ThemeParkStartPoint/src/test/java/behaviours/ISecurityTest.java
@@ -0,0 +1,17 @@
+package behaviours;
+
+import org.junit.Before;
+import org.junit.Test;
+import people.Visitor;
+
+public class ISecurityTest {
+    ISecurity iSecurity;
+
+    @Before
+    public void before(){
+        Visitor.visitor = new Visitor(14, 1.2, 40.0);
+    }
+
+    @Test
+    public void
+}
